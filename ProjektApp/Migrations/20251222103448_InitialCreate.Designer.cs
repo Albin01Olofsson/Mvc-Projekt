817626c61bf6260d8c9b7446667a1b028a72bf57
Albin01Olofsson <albin01olofsson@gmail.com>
2025-12-22 16:54:30 +0100

    main

--- ProjektApp/Migrations/20251222103448_InitialCreate.Designer.cs
+++ ProjektApp/Migrations/20251222103448_InitialCreate.Designer.cs
@@ -5,7 +5,8 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ProjektApp.Models;
+using Models;
+using DAL;
 
 #nullable disable
 
