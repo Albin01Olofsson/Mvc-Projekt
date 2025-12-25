66b289f1b0ecdd408966b303cb3d63a6520f859d
Albin01Olofsson <albin01olofsson@gmail.com>
2025-12-25 13:25:44 +0100

    Accountview/controller

--- ProjektApp/Controllers/AccountController.cs
+++ ProjektApp/Controllers/AccountController.cs
@@ -1,6 +1,83 @@
-﻿namespace ProjektApp.Controllers
+﻿using Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using ProjektApp.Viewmodels;
+
+
+namespace ProjektApp.Controllers
 {
-    public class AccountController
+    public class AccountController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = await _signInManager.PasswordSignInAsync(
+                model.Email,
+                model.Password,
+                model.RememberMe,
+                lockoutOnFailure: false);
+            if (result.Succeeded)
+                return RedirectToAction("Index", "Home");
+
+            ModelState.AddModelError("", "Felaktig inloggning");
+            return View(model);
+        }
+   
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model )
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var user = new User
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                IsActive = true
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }
+            foreach (var error in result.Errors)
+            
+            ModelState.AddModelError("", error.Description);
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
     }
+
+
 }
+
---
+++ ProjektApp/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using ProjektApp.Viewmodels;
+
+namespace ProjektApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [Authorize]
+        public IActionResult MyProfile()
+        {
+            return View();
+        }
+    }
+}
