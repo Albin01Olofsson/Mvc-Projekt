7a1a6c8b5231be31aadff5e17853c9a64dbe1014
Albin01Olofsson <albin01olofsson@gmail.com>
2026-01-09 16:23:53 +0100

    take home controller

--- ProjektApp/Controllers/HomeController.cs
+++ ProjektApp/Controllers/HomeController.cs
@@ -24,12 +24,13 @@ namespace ProjektApp.Controllers
             {
                 PublicProfiles = _context.Profile
                     .Where(p => isAuthenticated || !p.IsPrivate)
-                    .Take(5)
+                    .Take(8)
                     .ToList(),
 
                 CVs = _context.CVs
                     .Include(cv => cv.Profile)
                     .Where(cv => isAuthenticated || !cv.Profile.IsPrivate)
+                    .Take(6)
                     .ToList(),
 
                 LatestProjects = _context.Projects
